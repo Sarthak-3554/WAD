7775da451e3f4582140b0038a624943efb7e6d5b
Sarthak_D <sarthakdumbre55@gmail.com>
2024-05-03 22:29:04 +0530

    New changes

--- WADL/Student_Management/index.js
+++ WADL/Student_Management/index.js
@@ -47,7 +47,7 @@ app.get('/students', async (req, res) => {
     try {
       const students = await Student.find();
       // res.status(200).json({students})
-      res.render('students', { students }); // Assuming you're using a templating engine like EJS
+      res.render('student.ejs', { students }); // Assuming you're using a templating engine like EJS
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Server Error' });
